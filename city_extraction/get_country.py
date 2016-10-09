fdf95dd517f30192e17b3e1bc8003add98f8bd3e
Vinay Rao D <vinayraod@gmail.com>
2016-10-08 21:17:26 -0700

    Updated get_country
    Updated get_country to get the country for the most populous city

--- city_extraction/get_country.py
+++ city_extraction/get_country.py
@@ -3,23 +3,25 @@ import json
 import codecs
 import re
 
-#annotations_file = 'annotations_with_alt_1-25.json'
-annotations_file = 'annotations_with_alt_26-50.json'
-#annotations_file = 'annotations_with_text_1-25.json'
-#annotations_file = 'annotations_with_text_26-50.json'
 
+path = './'
+annotations_file = path + 'ann_city_title_state_1_25.json'
+#annotations_file = path + 'ann_city_title_state_26_50.json'
 
-#city_to_country_dict_file = 'city_country_dict_with_alt.json'
-city_to_country_dict_file = 'city_country_dict_alternates.json'
+city_to_country_dict_file = path + 'city_country_dict_15000_ordered_by_pop.json'
+state_to_country_dict_file = path + 'state_country_dict.json'
 
 city_to_country_dict = json.load(codecs.open(city_to_country_dict_file, 'r', 'utf-8'))
+state_to_country_dict = json.load(codecs.open(state_to_country_dict_file, 'r', 'utf-8'))
 
 city_to_country_dict_lower = {}
+state_to_country_dict_lower = {}
 
-for city,countries in city_to_country_dict.items():
-	city_to_country_dict_lower[city.lower()] = countries
+for city, countries in city_to_country_dict.items():
+	city_to_country_dict_lower[city.lower()] = countries[0][0]
 
-#city_to_country_dict = map(lambda x:x.lower(),city_to_country_dict)
+for state, countries in state_to_country_dict.items():
+	state_to_country_dict_lower[state.lower()] = countries
 
 count_of_zero_cities = 0
 count_of_single_cities = 0
@@ -32,14 +34,22 @@ count_total = 0
 def get_correct_cities(line):
 	return ast.literal_eval(line)['correct_cities']
 
+def get_correct_states(line):
+	return ast.literal_eval(line)['correct_states']
+
+def get_correct_cities_title(line):
+	return ast.literal_eval(line)['correct_cities_title']
+
 def get_correct_country(line):
 	return ast.literal_eval(line)['correct_country']
 
 with open(annotations_file, 'r') as f:
-	for index,line in enumerate(f):
+	for index, line in enumerate(f):
 		count_total += 1
 		#print(index)
 		correct_cities = get_correct_cities(line)
+		correct_states = get_correct_states(line)
+		correct_cities_title = get_correct_cities_title(line)
 		#print(correct_cities)
 		country_count_dict = {}
 		length = len(correct_cities)
@@ -51,7 +61,19 @@ with open(annotations_file, 'r') as f:
 		else:
 			count_of_multiple_cities += 1
 		for city in correct_cities:
-			countries = city_to_country_dict_lower[city]
+			country = city_to_country_dict_lower[city]
+			if country in country_count_dict:
+				country_count_dict[country] += 1
+			else:
+				country_count_dict[country] = 1
+		for city in correct_cities_title:
+			country = city_to_country_dict_lower[city]
+			if country in country_count_dict:
+				country_count_dict[country] += 1
+			else:
+				country_count_dict[country] = 1
+		for state in correct_states:
+			countries = state_to_country_dict_lower[state]
 			distinct_countries = set(countries)
 			for country in distinct_countries:
 				if country in country_count_dict:
@@ -73,6 +95,8 @@ with open(annotations_file, 'r') as f:
 			print("Correct Find")
 			count_of_correct_finds += 1
 			print(correct_cities)
+			print(correct_states)
+			print(correct_cities_title)
 			print(country_count_dict)
 			print(countrymax)
 			print(get_correct_country(line))
@@ -80,6 +104,8 @@ with open(annotations_file, 'r') as f:
 			print("No Distinct Max")
 			count_of_no_distinct += 1
 			print(correct_cities)
+			print(correct_states)
+			print(correct_cities_title)
 			print(country_count_dict)
 			print("Predicted:{}".format(countrymax))
 			print("Correct:{}".format(get_correct_country(line)))
@@ -87,15 +113,17 @@ with open(annotations_file, 'r') as f:
 			print("Incorrect Find")
 			count_of_incorrect_finds += 1
 			print(correct_cities)
+			print(correct_states)
+			print(correct_cities_title)
 			print(country_count_dict)
 			print("Predicted:{}".format(countrymax))
 			print("Correct:{}".format(get_correct_country(line)))
 		print('---------------------------')
 
 print("count_total:{}".format(count_total))
-print("count_of_correct_finds:{} [{}%]".format(count_of_correct_finds, count_of_correct_finds/count_total*100))
-print("count_of_incorrect_finds:{} [{}%]".format(count_of_incorrect_finds, count_of_incorrect_finds/count_total*100))
-print("count_of_multiple_max_cities:{} [{}%]".format(count_of_no_distinct, count_of_no_distinct/count_total*100))
-print("count_of_zero_cities:{} [{}%]".format(count_of_zero_cities, count_of_zero_cities/count_total*100))
-print("count_of_single_cities:{} [{}%]".format(count_of_single_cities, count_of_single_cities/count_total*100))
-print("count_of_multiple_cities:{} [{}%]".format(count_of_multiple_cities, count_of_multiple_cities/count_total*100))
+print("count_of_correct_finds:{} [{}%]".format(count_of_correct_finds, (count_of_correct_finds*1.0/count_total)*100))
+print("count_of_incorrect_finds:{} [{}%]".format(count_of_incorrect_finds, (count_of_incorrect_finds*1.0/count_total)*100))
+print("count_of_multiple_max_cities:{} [{}%]".format(count_of_no_distinct, count_of_no_distinct*1.0/count_total*100))
+print("count_of_zero_cities:{} [{}%]".format(count_of_zero_cities, count_of_zero_cities*1.0/count_total*100))
+print("count_of_single_cities:{} [{}%]".format(count_of_single_cities, count_of_single_cities*1.0/count_total*100))
+print("count_of_multiple_cities:{} [{}%]".format(count_of_multiple_cities, count_of_multiple_cities*1.0/count_total*100))
